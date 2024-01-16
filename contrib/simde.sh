1404191fb50310be715e0e7924614371f213c02c
Tino Reichardt <milky-zfs@mcmilk.de>
2024-01-16 23:27:03 +0100

    Change parameter of compiling:
    Remove "-mbranch-protection=standard" - this will break gcc-8 + gcc-9
    
    Signed-off-by: Tino Reichardt <milky-zfs@mcmilk.de>

--- contrib/simde.sh
+++ contrib/simde.sh
@@ -6,20 +6,26 @@ test -d simde || git clone https://github.com/simd-everywhere/simde
 # cleanup old stuff
 rm -f *.S
 
-# bsd/macos: -ffixed-x18
-# macos:     -fno-omit-frame-pointer
-OPTS="-Ofast -Wall -Wextra -std=c99 -fno-omit-frame-pointer -fno-verbose-asm -DSIMDE_ENABLE_NATIVE_ALIASES -fno-addrsig -ffixed-x18 -mbranch-protection=standard"
+OPTS="-Ofast -Wall -Wextra -std=c99 -fno-omit-frame-pointer -fno-verbose-asm -DSIMDE_ENABLE_NATIVE_ALIASES"
+CC="gcc"
+CC="clang"
 
-CC="gcc13"
-CC="clang14"
+# needed for clang
+OPTS="$OPTS -fno-addrsig"
 
-T="--target=aarch64-freebsd-gnu"
+# when using this, we will break gcc-8 and gcc-9
+# OPTS="$OPTS -mbranch-protection=standard"
+
+# on bsd/macos x18 is used by kernel
+OPTS="$OPTS -ffixed-x18"
+
+# needed for macos
+OPTS="$OPTS -fno-omit-frame-pointer"
+
 #T="--target=aarch64-linux-gnu"
+T="--target=aarch64-freebsd-gnu"
 A="-march=armv8-a"
 
 # best results with clang -O3:
 $CC $OPTS $A $T -S blake3_sse2.c  -o b3_aarch64_sse2.S  2>b3_aarch64_sse2.err
 $CC $OPTS $A $T -S blake3_sse41.c -o b3_aarch64_sse41.S 2>b3_aarch64_sse41.err
-
-# avx2 does not bring speed ups on that arch
-#clang $OPTS $A $T -S blake3_avx2.c -o b3_aarch64_avx2.S   2>b3_aarch64_avx2.err
